5392c96a0365f5eb4f7638e38213961dcde0a4fb
Z1llya <zubalii.illya@gmail.com>
2022-06-22 15:20:52 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -7,17 +7,53 @@ function App() {
 
   return (
     <div >
-        {
-           <Character
-               id={'1'}
-               name={'Rick Sanchez'}
-               status ={'Alive'}
-               species={'Human'}
-               gender ={'male'}
-               image = {"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}
+      {
+        <Character
+            id={'1'}
+            name={'Rick Sanchez'}
+            status={'Alive'}
+            species={'Human'}
+            gender={'male'}
+            image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}
 
-           />
-        }
+        />
+      }
+      {
+        <Character
+            id={'2'}
+            name={'Morty Smith'}
+            status={'Alive'}
+            species={'Human'}
+            gender={'male'}
+            image={"https://rickandmortyapi.com/api/character/avatar/2.jpeg"}
+
+        />
+      }
+      {
+        <Character
+            id={'3'}
+            name={'Summer Smith'}
+            status={'Alive'}
+            species={'Human'}
+            gender={'female'}
+            image={"https://rickandmortyapi.com/api/character/avatar/3.jpeg"}
+
+        />
+      }
+      {
+        <Character
+            id={'4'}
+            name={'Beth Smith'}
+            status={'Alive'}
+            species={'Human'}
+            gender={'female'}
+            image={"https://rickandmortyapi.com/api/character/avatar/4.jpeg"}
+
+        />
+      }
+
+
+
 
 
     </div>
